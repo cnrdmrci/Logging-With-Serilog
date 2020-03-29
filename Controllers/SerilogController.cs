0cf6ab85330a1dd0f90fc22aee0f3f23c8d4b713
cnrdmrci <canerdemirci.mail@gmail.com>
2020-03-29 14:33:29 +0300

    project created

---
+++ Controllers/SerilogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingWithSerilog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SerilogController : ControllerBase
+    {
+        private readonly ILogger<SerilogController> _logger;
+
+        public SerilogController(ILogger<SerilogController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}

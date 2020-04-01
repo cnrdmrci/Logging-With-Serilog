3c5a3ae1db45ac4e0c01c50d46ba5e7dc8d5d22a
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-01 14:02:51 +0300

    Serilog kullanım örnekleri oluşturuldu.

--- Controllers/SerilogController.cs
+++ Controllers/SerilogController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace LoggingWithSerilog.Controllers
 {
@@ -16,6 +17,56 @@ namespace LoggingWithSerilog.Controllers
         public SerilogController(ILogger<SerilogController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpGet("DependencyTest")]
+        public IActionResult DependencyTest()
+        {
+            // StartUp ve Program classlarındaki tanımlamayı baz alarak log ataması yapıldı.
+            var person = new { Name = "Caner", City = "İstanbul" };
+            _logger.LogWarning("Test Dependency, {@Person}", person);
+            _logger.LogTrace("Verbose log");
+            Log.Logger.ForContext("Tip","Genel").Information("Info log");
+            return Ok();
+        }
+
+        [HttpGet("ConsoleTest")]
+        public IActionResult WriteToConsoleTest()
+        {
+            //Local olarak tanımladık. Konsol uygulamalarında kullanılabilir.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            var person = new { Name = "Caner", City = "İstanbul" };
+            Log.Logger.ForContext("Tip", "Console").Warning("Test Console, {@Person}", person);
+            
+            return Ok();
+        }
+
+        [HttpGet("SqlServerTest")]
+        public IActionResult WriteToSqlServerTest()
+        {
+            //Local olarak tanımladık. Konsol uygulamalarında kullanılabilir.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.MSSqlServer("Server=localhost;Database=SeriLog;User Id=userName;Password=password;", "Logs")
+                .CreateLogger();
+            var person = new { Name = "Caner", City = "İstanbul" };
+            Log.Logger.ForContext("Tip", "SqlServer").Warning("Test SqlServer {@Person}", person);
+            return Ok();
+        }
+
+        [HttpGet("Seq")]
+        public IActionResult TestSeq()
+        {
+            //Local olarak tanımladık. Konsol uygulamalarında kullanılabilir.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Seq("http://localhost:5341")
+                .WriteTo.Console()
+                .CreateLogger();
+            var person = new { Name = "Caner", City = "İstanbul" };
+            Log.Logger.ForContext("Tip", "Seq").Warning("Test Seq {@Person}", person);
+
+            return Ok();
         }
     }
 }

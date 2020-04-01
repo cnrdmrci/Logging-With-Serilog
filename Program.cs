1e326b29816ccf581405d98e1c54e213fef15b68
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-01 14:01:59 +0300

    serilog startup classında sisteme entegre edildi.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace LoggingWithSerilog
 {
@@ -13,11 +15,37 @@ namespace LoggingWithSerilog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Serilog ayarlarý ve tanýmlama iþlemleri oluþturuldu.
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("LoggingWithSerilog.Controllers", LogEventLevel.Verbose)
+                .Enrich.WithProperty("AppName", "Serilog Sample")
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+
+            // try-catch içerisinde çalýþacak ve herhangi bir çökme veya hata durumunda loglanacak.
+            try
+            {
+                Log.Information("Starting host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog() // Serilogun kullanýlacaðý belirtildi.
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
